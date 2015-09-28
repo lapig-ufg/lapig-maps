95477c3c0af1e1883b010748be76c0dc1ebc33b9
Leandro Leal Parente <leal.parente@gmail.com>
2015-09-28 19:19:40 -0300

    Improviments in Lapig Spatial Intelligence Tool

---
+++ src/client/app/src/pastagem/tools/LapigSpatialIntelligenceBtn.js
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2008-2011 The Open Planning Project
+ * 
+ * Published under the GPL license.
+ * See https://github.com/opengeo/gxp/raw/master/license.txt for the full text
+ * of the license.
+ */
+
+/**
+ * @requires plugins/Tool.js
+ */
+
+Ext.namespace("lapig.tools");
+
+lapig.tools.LapigSpatialIntelligenceBtn = Ext.extend(gxp.plugins.Tool, {
+    
+    
+    ptype: "lapig_spatialintelligencebtn",
+    menuText: 'Análise de Informações Geoǵráficas',
+    actionTip: 'Análise de Informações Geoǵráficas',
+
+    addActions: function() {
+        var actions = lapig.tools.LapigSpatialIntelligenceBtn.superclass.addActions.apply(this, [{
+            menuText: this.menuText,
+            tooltip: this.actionTip,
+            icon   : 'theme/app/img/spatial-intelligence.png',
+            handler: function() {
+                Ext.getCmp('eastpanel').expand(true);
+            },
+            scope: this
+        }]);
+        
+        return actions;
+    }
+        
+});
+
+Ext.preg(lapig.tools.LapigSpatialIntelligenceBtn.prototype.ptype, lapig.tools.LapigSpatialIntelligenceBtn);
