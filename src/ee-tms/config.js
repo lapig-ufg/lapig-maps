0a90985cf69945ce0567fd2019cd94963ceee3df
jose <jose@localhost.localdomain>
2016-02-01 18:25:56 -0200

    Update for work in home.

--- src/ee-tms/config.js
+++ src/ee-tms/config.js
@@ -5,7 +5,9 @@ module.exports = function(app) {
 	var pathTmp = '/mnt/tmpfs/';
 
 	var config = {
-			  'redisHost': 'localhost'
+				'appRoot': appRoot
+			,	'pathCreateMapID':appRoot+'/integration/py/create_mapid.py'
+			, 'redisHost': 'localhost'
 			,	'port': 5000
 			, 'pathXML': appRoot+'/Template/wmts.xml'
 			, 'eeTms': "https://earthengine.googleapis.com"
@@ -15,7 +17,7 @@ module.exports = function(app) {
 						"temporal_resolution": "16",
 				    "temporal_resolution_type": "day",
 				    "start_date": "2014/01/01",
-				    "end_date": "2014/01/17",
+				    "end_date": "2014/04/01",
 				    "collection_id": "LANDSAT/LC8_L1T_TOA",
 				    "composites": ["B6,B5,B4"],
 				    "b_box": ["-76.60","-35.06","-33.43","5.13"],
