400ec5bc0ff371a21369cd4a75bca813cb9dffd8
Leandro <leandro@localhost.localdomain>
2016-03-29 15:36:52 -0300

    Compartilhamento de sessão entre instâncias do nodeJs

--- src/server/integration/sh/pre-process/run.d/001all/01-remove-dummy-files.sh
+++ src/server/integration/sh/pre-process/run.d/001all/01-remove-dummy-files.sh
@@ -1,1 +1,10 @@
-../../routines.d/all/remove-dummy-files.sh
+filename=$(basename $filepath)
+
+log_same "PROCESSED $FILES_COUNT FILES"
+
+if [ -z "$(echo ${filename##*.} | grep 'shp\|shx\|dbf\|prj\|qix\|tif\|tfw\|sld\|map\|kml\|kmz\|png\|jpg\|pdf\|.tif.aux.xml')" ]; then
+	log "DELETE: $filename"
+	if [[ $MODE = 'APPLY' ]]; then
+		rm -f $filepath
+	fi
+fi 
--- src/server/integration/sh/pre-process/run.d/001all/02-rename-files.sh
+++ src/server/integration/sh/pre-process/run.d/001all/02-rename-files.sh
@@ -1,1 +1,11 @@
-../../routines.d/all/rename-files.sh
+new_filename=$(echo $filename | tr '[:upper:]' '[:lower:]')
+new_filepath=$( echo $filepath | sed s/$filename/$new_filename/g)
+
+log_same "PROCESSED $FILES_COUNT FILES"
+
+if [ "$filename" != "$new_filename" ]; then
+	log "RENAME: $filename => $new_filename "
+	if [[ $MODE = 'APPLY' ]]; then
+		mv $filepath $new_filepath
+	fi
+fi
