802a352f2aa4f2217a0859881ff5495344c2bb56
Guilherme F. A. Souza <guifas3000@gmail.com>
2016-04-12 13:49:21 -0300

    Adicionado shell script para inicializar o ambiente de desenvolvimento

---
+++ devops/scripts/start_lapig-maps_ambiente_desenvolvimento.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#Programa para inicializar automaticamente os serviços necessários para executar o ambiente Lapig-maps.
+
+MONGOPS=0
+REDISPS=0
+SERVERPS=0
+CLIENTPS=0
+
+terminate(){
+	echo -e '\nWill terminate now.\n'
+	echo "mongo_pid: $MONGOPS; redis_pid: $REDISPS; server_pid: $SERVERPS; client_pid: $CLIENTPS"
+	kill $MONGOPS
+	kill $REDISPS
+	kill $SERVERPS
+	kill $CLIENTPS
+	exit 0
+}
+
+trap terminate SIGHUP SIGINT SIGTERM
+
+echo 'Starting MONGO'
+mongod 2> mongo_err.log &
+MONGOPS=$!
+sleep 4
+
+echo 'Starting REDIS'
+redis-server 2> redis_err.log &
+REDISPS=$!
+sleep 2
+
+echo 'Starting CLIENT'
+pushd 'lapig-maps/src/client'
+./start.sh 2> client_err.log &
+CLIENTPS=$!
+sleep 6
+popd
+
+echo 'Starting SERVER'
+pushd 'lapig-maps/src/server'
+./start.sh &
+SERVERPS=$!
+sleep 1
+echo "mongo_pid: $MONGOPS; redis_pid: $REDISPS; server_pid: $SERVERPS; client_pid: $CLIENTPS"
+
+wait
