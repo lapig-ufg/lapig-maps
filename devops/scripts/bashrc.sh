85cf603ab32ca78bfadd86ba8904148a80a5cd7f
Leandro <leandro@localhost.localdomain>
2016-09-10 10:27:13 -0300

    devops updates

--- devops/scripts/bashrc.sh
+++ devops/scripts/bashrc.sh
@@ -6,18 +6,31 @@ NODE_VERSION=0.12.2
 DEV_DIR=/data/lapig-maps/dev
 PROD_DIR=/data/lapig-maps/prod
 
+OWS_PID_PATTERN='/data/lapig-maps/prod/ows/app-cluster.js'
+SERVER_PID_PATTERN='/data/lapig-maps/prod/server/app-cluster.js'
+
+clear-cache() {
+    redis-cli -h lapig-redis keys "*$1*" | xargs redis-cli DEL
+}
+
 alias update='cd $REPOSITORY_DIR; git pull'
 alias deploy='$REPOSITORY_DIR/devops/scripts/deploy.sh $@'
 
 alias prod='cd $PROD_DIR'
 alias rep='cd $REPOSITORY_DIR'
 
-alias ows-start="su -c 'svc -u /etc/service/ows/'"
-alias ows-stop="su -c 'svc -d /etc/service/ows/'"
+alias ows-clear-cache=clear-cache
+alias ows-pid='pgrep -f $OWS_PID_PATTERN'
+alias ows-restart="sudo pgrep -f $$OWS_PID_PATTERN | xargs kill"
+alias ows-start="sudo svc -u /etc/service/ows/"
+alias ows-stop="sudo svc -d /etc/service/ows/; sudo pgrep -f $OWS_PID_PATTERN | xargs kill"
 alias ows-log="tail -f /var/log/lapig-maps/ows.log"
+alias ows-mapserver-log="tail -f /var/log/lapig-maps/mapserver.log"
 
-alias server-start="su -c 'svc -u /etc/service/server/'"
-alias server-stop="su -c 'svc -d /etc/service/server/'"
+alias server-pid='pgrep -f $SERVER_PID_PATTERN'
+alias server-restart="sudo pgrep -f $SERVER_PID_PATTERN | xargs kill"
+alias server-start="sudo svc -u /etc/service/server/"
+alias server-stop="sudo svc -d /etc/service/server/; pgrep -f $SERVER_PID_PATTERN | xargs kill"
 alias server-log="tail -f /var/log/lapig-maps/server.log"
 
 [[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh # This loads NVM
--- src/ows/config.js
+++ src/ows/config.js
@@ -47,7 +47,7 @@ module.exports = function(app) {
 
 	if(process.env.NODE_ENV == 'prod') {
 		config['path_fonts_list'] =  appRoot + '/data_dir/ows_fonts_prod.list';
-		config['path_logfile'] = '/var/log/lapig-maps/ows.log'
+		config['path_logfile'] = '/var/log/lapig-maps/mapserver.log'
 		config['path_mapserv'] = 'mapserv'
 		config['ows_onlineresource'] = 'http://maps.lapig.iesa.ufg.br/ows';
 		config['redisHost'] = '200.137.217.157'
