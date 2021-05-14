330a43ffc5d94da423e211584037153047db0e67
Tharles de Sousa Andrade <irtharles@gmail.com>
2021-05-14 17:22:17 -0300

    add plugin logos

---
+++ src/client/app/src/pastagem/tools/LapigLogos.js
@@ -0,0 +1,53 @@
+/**
+ * @requires plugins/Tool.js
+ */
+
+Ext.namespace("lapig.tools");
+
+lapig.tools.logos = Ext.extend(gxp.plugins.Tool, {
+
+    ptype: "lapig_logos",
+
+    constructor: function(config) {
+        lapig.tools.logos.superclass.constructor.apply(this, arguments);
+    },
+
+    addOutput: function(config) {
+        config = Ext.apply(this.createOutputConfig(), config || {});
+        var output = lapig.tools.logos.superclass.addOutput.call(this, config);
+
+        return output;
+    },
+
+    createOutputConfig: function() {
+        return  {
+            xtype: 'panel',
+            border: false,
+            region: "west",
+            layout: {
+                type: 'hbox',
+                align: 'center',
+                pack: 'center'
+            },
+            margin: 20,
+            padding: 20,
+            items: [
+                {
+                    xtype: 'box',
+                    margins: {top:15, left:0, right: 5, bottom:0},
+                    width: 100,
+                    height: 35,
+                    cls: 'lapig',
+                },
+                {
+                    xtype: 'box',
+                    width: 70,
+                    height: 70,
+                    cls: 'ufg',
+                },
+            ]
+        };
+    },
+});
+
+Ext.preg(lapig.tools.logos.prototype.ptype, lapig.tools.logos);
--- src/client/app/src/pastagem/tools/LapigViewer.js
+++ src/client/app/src/pastagem/tools/LapigViewer.js
@@ -45,6 +45,7 @@
  * @requires tools/LapigDownloadAll.js
  * @requires tools/LapigLogin.js
  * @requires tools/LapigUtils.js
+ * @requires tools/LapigLogos.js
  */
 
 globalInstance = this;
@@ -309,31 +310,12 @@ gxp.LapigViewer = Ext.extend(gxp.Viewer, {
           tools: [
             /********** Border layout regions */
             {
-              xtype: 'panel',
-              border: false,
-              region: "west",
-              layout: {
-                  type: 'hbox',
-                  align: 'center',
-                  pack: 'center'
-              },
-              margin: 20,
-              padding: 20,
-              items: [
-                  {
-                      xtype: 'box',
-                      margins: {top:15, left:0, right: 5, bottom:0},
-                      width: 100,
-                      height: 35,
-                      cls: 'lapig',
-                  },
-                  {
-                      xtype: 'box',
-                      width: 70,
-                      height: 70,
-                      cls: 'ufg',
-                  },
-              ]
+              ptype: "lapig_logos",
+              outputTarget: "westpanel",
+              project: project,
+              outputConfig: {
+                  height: 200
+              }
             },
             {
               ptype: "gxp_lapiglayermanager",
