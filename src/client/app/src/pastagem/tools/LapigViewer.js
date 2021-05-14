69c15eee471b5f53513d4eb0b94187cc8722d350
Tharles de Sousa Andrade <irtharles@gmail.com>
2021-05-14 16:10:35 -0300

    add logos 04

--- src/client/app/src/pastagem/tools/LapigViewer.js
+++ src/client/app/src/pastagem/tools/LapigViewer.js
@@ -265,36 +265,7 @@ gxp.LapigViewer = Ext.extend(gxp.Viewer, {
                   collapsible: true,
                   collapseMode: "mini",
                   header: false,
-                  autoScroll: true,
-                  items: [
-                    {
-                        xtype: 'panel',
-                        border: false,
-                        region: "west",
-                        layout: {
-                            type: 'hbox',
-                            align: 'center',
-                            pack: 'center'
-                        },
-                        margin: 20,
-                        padding: 20,
-                        items: [
-                            {
-                                xtype: 'box',
-                                margins: {top:15, left:0, right: 5, bottom:0},
-                                width: 100,
-                                height: 35,
-                                cls: 'lapig',
-                            },
-                            {
-                                xtype: 'box',
-                                width: 70,
-                                height: 70,
-                                cls: 'ufg',
-                            },
-                        ]
-                    }
-                  ]
+                  autoScroll: true
                 },
                 {
                   id: "southpanel",
