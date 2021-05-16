1edcacdd9e2fd742d0cd514b8ae4cd67524ce9f3
luizmlpascoal <luizmlpascoal@gmail.com>
2021-05-16 20:00:05 -0300

    nvi08

--- src/client/app/src/pastagem/tools/LapigRasterSeries.js
+++ src/client/app/src/pastagem/tools/LapigRasterSeries.js
@@ -863,6 +863,7 @@ lapig.tools.RasterSeries = Ext.extend(gxp.plugins.Tool, {
                             }
                         }
                     }
+
                     ],
                     items: [{
                         region: 'center',
@@ -876,7 +877,9 @@ lapig.tools.RasterSeries = Ext.extend(gxp.plugins.Tool, {
                             new Ext.ux.Chartjs({
                                 type: "Line",
                                 data: {
-                                    labels: ["January", "February", "March", "April", "May", "June", "July"],
+                                    labels: instance.MyMap(instance.globalChartData, function (element) {
+                                        return new Date(element.dateStr).format("d/m/Y")
+                                    }),
                                     datasets: [{
                                         label: "Modis-Original",
                                         fillColor: "rgba(220,220,220,0.2)",
@@ -885,18 +888,20 @@ lapig.tools.RasterSeries = Ext.extend(gxp.plugins.Tool, {
                                         pointStrokeColor: "#fff",
                                         pointHighlightFill: "#fff",
                                         pointHighlightStroke: "rgba(220,220,220,1)",
-                                        data: [65, 59, 80, 81, 56, 55, 40]
-                                    },
-                                    {
-                                        label: "My Second dataset",
-                                        fillColor: "rgba(151,187,205,0.2)",
-                                        strokeColor: "rgba(151,187,205,1)",
-                                        pointColor: "rgba(151,187,205,1)",
-                                        pointStrokeColor: "#fff",
-                                        pointHighlightFill: "#fff",
-                                        pointHighlightStroke: "rgba(151,187,205,1)",
-                                        data: [28, 48, 40, 19, 86, 27, 90]
+                                        data: instance.MyMap(instance.globalChartData, function (element) {
+                                            return element.original
+                                        })
                                     }
+                                        // {
+                                        //     label: "My Second dataset",
+                                        //     fillColor: "rgba(151,187,205,0.2)",
+                                        //     strokeColor: "rgba(151,187,205,1)",
+                                        //     pointColor: "rgba(151,187,205,1)",
+                                        //     pointStrokeColor: "#fff",
+                                        //     pointHighlightFill: "#fff",
+                                        //     pointHighlightStroke: "rgba(151,187,205,1)",
+                                        //     data: [28, 48, 40, 19, 86, 27, 90]
+                                        // }
                                     ]
                                 }
                             })
