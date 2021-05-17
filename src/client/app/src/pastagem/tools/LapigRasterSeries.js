663675973b6a0ad5ad8be6dedffee6b7fc041375
Tharles de Sousa Andrade <irtharles@gmail.com>
2021-05-17 11:49:06 -0300

    update config chartjs 02

--- src/client/app/src/pastagem/tools/LapigRasterSeries.js
+++ src/client/app/src/pastagem/tools/LapigRasterSeries.js
@@ -22,33 +22,8 @@ lapig.tools.RasterSeries = Ext.extend(gxp.plugins.Tool, {
 
     chartJS: new Ext.ux.Chartjs({
         type: "line",
-        data: {
-            // labels: ["January", "February", "March", "April", "May", "June", "July"],
-            // datasets: [{
-            //     label: "My First dataset",
-            //     fillColor: "rgba(220,220,220,0.2)",
-            //     strokeColor: "rgba(220,220,220,1)",
-            //     pointColor: "rgba(220,220,220,1)",
-            //     pointStrokeColor: "#fff",
-            //     pointHighlightFill: "#fff",
-            //     pointHighlightStroke: "rgba(220,220,220,1)",
-            //     data: [65, 59, 80, 81, 56, 55, 40]
-            // },
-            // {
-            //     label: "My Second dataset",
-            //     fillColor: "rgba(151,187,205,0.2)",
-            //     strokeColor: "rgba(151,187,205,1)",
-            //     pointColor: "rgba(151,187,205,1)",
-            //     pointStrokeColor: "#fff",
-            //     pointHighlightFill: "#fff",
-            //     pointHighlightStroke: "rgba(151,187,205,1)",
-            //     data: [28, 48, 40, 19, 86, 27, 90]
-            // }
-            // ]
-        },
-        options: {
-            responsive: true
-        }
+        data: {},
+        options: {}
     }),
 
     data: null,
@@ -453,12 +428,8 @@ lapig.tools.RasterSeries = Ext.extend(gxp.plugins.Tool, {
             labels: t1,
             datasets: [{
                 label: "Modis-Original",
-                // fillColor: "rgba(224, 63, 63.2)",
-                strokeColor: "rgba(224, 63, 63,1)",
-                pointColor: "rgba(224, 63, 63,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(224, 63, 63,1)",
+                borderColor: "#3e95cd",
+                fill: false,
                 data: t2
             }]
         })
