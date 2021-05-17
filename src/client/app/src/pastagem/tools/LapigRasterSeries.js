82309dbb3ae57b6530b0092f773311c0acd32bd0
Tharles de Sousa Andrade <irtharles@gmail.com>
2021-05-17 11:05:53 -0300

    change constructor of chartjs 03

--- src/client/app/src/pastagem/tools/LapigRasterSeries.js
+++ src/client/app/src/pastagem/tools/LapigRasterSeries.js
@@ -21,30 +21,30 @@ lapig.tools.RasterSeries = Ext.extend(gxp.plugins.Tool, {
     globalChartData: [],
 
     chartJS: new Ext.ux.Chartjs({
-        type: "Line",
+        type: "line",
         data: {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
-            datasets: [{
-                label: "My First dataset",
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [65, 59, 80, 81, 56, 55, 40]
-            },
-            {
-                label: "My Second dataset",
-                fillColor: "rgba(151,187,205,0.2)",
-                strokeColor: "rgba(151,187,205,1)",
-                pointColor: "rgba(151,187,205,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(151,187,205,1)",
-                data: [28, 48, 40, 19, 86, 27, 90]
-            }
-            ]
+            // labels: ["January", "February", "March", "April", "May", "June", "July"],
+            // datasets: [{
+            //     label: "My First dataset",
+            //     fillColor: "rgba(220,220,220,0.2)",
+            //     strokeColor: "rgba(220,220,220,1)",
+            //     pointColor: "rgba(220,220,220,1)",
+            //     pointStrokeColor: "#fff",
+            //     pointHighlightFill: "#fff",
+            //     pointHighlightStroke: "rgba(220,220,220,1)",
+            //     data: [65, 59, 80, 81, 56, 55, 40]
+            // },
+            // {
+            //     label: "My Second dataset",
+            //     fillColor: "rgba(151,187,205,0.2)",
+            //     strokeColor: "rgba(151,187,205,1)",
+            //     pointColor: "rgba(151,187,205,1)",
+            //     pointStrokeColor: "#fff",
+            //     pointHighlightFill: "#fff",
+            //     pointHighlightStroke: "rgba(151,187,205,1)",
+            //     data: [28, 48, 40, 19, 86, 27, 90]
+            // }
+            // ]
         },
         options: {
             responsive: true
@@ -993,6 +993,7 @@ lapig.tools.RasterSeries = Ext.extend(gxp.plugins.Tool, {
                         xtype: 'panel',
                         disabled: true,
                         layout: 'fit',
+                        height: 283,
                         canResize: true,
                         padding: 10,
                         id: 'lapig-raster-series-tab-series-chart-pnl',
