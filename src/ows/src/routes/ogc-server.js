6784971c7e55e814642afdffd1be1b5c628654a5
Tharles de Sousa Andrade <irtharles@gmail.com>
2021-07-15 13:59:20 -0300

    Update ogc-server.js
    added filter urls

--- src/ows/src/routes/ogc-server.js
+++ src/ows/src/routes/ogc-server.js
@@ -1,8 +1,30 @@
 module.exports = function (app) {
 
-	var ogcServer = app.controllers.ogcServer;
-	
-	app.get('/ows', ogcServer.ows);
+	const ogcServer = app.controllers.ogcServer;
+
+	const filterUrl = async function (request, response,  next) {
+		try {
+		    const params = request.query; 
+
+		    if ('OUTPUTFORMAT' in params){
+
+			if(params.TYPENAME === 'pasture_quality' && params.OUTPUTFORMAT === 'shape-zip') {
+				response.status(403).json({ ok: false, message: 'resource not allowed' });
+			} else {
+				next()
+			}
+
+		    } else {
+			next()
+		    }
+
+		}catch (e) {
+		    console.error(e)
+		    response.status(400).json({ auth: false, message: 'Failed to proccess your resquest!' });
+		}
+	}
+
+	app.get('/ows', filterUrl, ogcServer.ows);
 	app.get('/sld/:filename', ogcServer.sld);
 
 	app.get('/', function(req, res) { res.end(); });
