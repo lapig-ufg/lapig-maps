68748519cdddb85d7ab45aa3176dae71752040ff
Tharles de Sousa Andrade <irtharles@gmail.com>
2022-12-03 19:33:53 -0300

    remove client 01

--- src/server/routes/root.js
+++ src/server/routes/root.js
@@ -1,7 +1,7 @@
 module.exports = function (app) {
 
 	app.get('/', function(req, res) {
-		response.send({ "server": "LAPIG-MAPS Server API", "ststus": "runing"});
+		res.send({ "server": "LAPIG-MAPS Server API", "ststus": "runing"});
 		res.end();
 	});
 
